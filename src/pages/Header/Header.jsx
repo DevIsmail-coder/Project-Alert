381dddcd0a1b00ab2afb1f41ab38ed80180d008c
DevIsmail-coder <ismailolawale777@gmail.com>
2025-02-27 08:20:29 +0100

    Refactor Header and Get Involved components for improved navigation and styling

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import './Header.css'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+const location = useLocation()
+  const navigate = useNavigate()
+
+  const getActivediv = (path) => {
+    return location.pathname === path ? "active" : "";
+  };
   return (
     <div className='Headerbody'>
       <article className='Headerdiv'>
         <h3 className='Headerdivh3'>Project Alert</h3>
       </article>
-      <article className='Headerdiv1'>
-        <p>Home</p>
-        <p>About us</p>
-        <p>Get Involved</p>
-        <p>Blog</p>
-        <p>Contact us</p>
+      <article  className='Headerdiv1'>
+        <div className={`Headerdiv1span ${getActivediv("/")}`} onClick={() => navigate("/")}>
+          <p className='Headerline'  >Home</p>
+          {/* <span className='Headerline'></span> */}
+        </div>
+        <div className={`Headerdiv1span ${getActivediv("/aboutus")}`} onClick={() => navigate("/aboutus")}>
+          <p className='Headerline'>About us</p>
+          {/* <span></span> */}
+          </div>
+        <div className={`Headerdiv1span ${getActivediv("/getinvolved")}`} onClick={() => navigate("/getinvolved")}>
+          <p className='Headerline'>Get Involved</p>
+          {/* <span></span> */}
+        </div>
+        <div className={`Headerdiv1span ${getActivediv("/blog")}`} onClick={() => navigate("/blog")}>
+          <p className='Headerline'>Blog</p>
+          {/* <span></span> */}
+        </div>
+        <div className={`Headerdiv1span ${getActivediv("/contactus")}`} onClick={() => navigate("/contactus")}>
+          <p className='Headerline'>Contact us</p>
+          {/* <span></span> */}
+        </div>
       </article>
       <article className='Headerdiv'>
         <button className='Headerbutton'>Report a case</button>
